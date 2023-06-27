cb3f11a2aec0143ae7ead908ac25690b91223745
narendrapujanaboyina <119730895+narendrapujanaboyina@users.noreply.github.com>
2023-06-27 16:53:08 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,10 @@
+def Buildapp(){
+  echo "Hello ${NAME} if want any chacoletes like ${params.chacolete}"
+}
+def Testapp(){
+  echo "testing the application"
+}
+def Deployapp(){
+  echo "Successfully deployed the app"
+}
+return this
